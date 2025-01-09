69147d8e15050244098bc60f07fe1527dae174d8
Neha <nehasurati1605@gmail.com>
2025-01-09 17:21:34 +0530

    update cluster

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose= require("mongoose");
 
 const database= async()=>{
    try {
-    await mongoose.connect("mongodb+srv://nehasurati1605:nehashish@cluster0.1j1m1ey.mongodb.net/Moviedb");
+    await mongoose.connect("mongodb+srv://nehasurati1605:nehashish@myfreecluster.tbrc3.mongodb.net/?retryWrites=true&w=majority&appName=MyFreeCluster");
     console.log("Database connected..!")
    } catch (error) {
     console.log(error);
